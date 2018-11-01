db0504a4ac1ab540fe91a6ed42877b1781225ef8
Nate Miller <natekmiller96@gmail.com>
2018-11-01 03:32:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 	require_all 'app'
+	require_all 'app/helpers'
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
@@ -11,4 +12,46 @@ class ApplicationController < Sinatra::Base
   get '/' do
 		erb :index
 	end
+	get '/signup' do
+		if Helpers.is_logged_in?(session)
+			redirect to '/tweets'
+		else
+			erb :'users/create_user', :layout => false
+		end
+  end
+  post "/signup" do
+    @user = User.create(:username => params[:username], :password => params[:password], :email => params[:email])
+		if @user.save
+	    session[:user_id] = @user.id
+			redirect to '/tweets'
+		else
+			redirect to '/signup'
+		end
+  end
+  get '/logout' do
+		if !Helpers.is_logged_in?(session)
+			redirect to '/login'
+		else
+	    session.clear
+	    redirect to '/login'
+		end
+  end
+
+  get '/login' do
+		if Helpers.is_logged_in?(session)
+			redirect to '/tweets'
+		else
+    	erb :'users/login', :layout => false
+		end
+  end
+  post '/login' do
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect to '/tweets'
+      else
+        @fail = true
+        erb :'/users/login', :layout => false
+      end
+  end
 end
